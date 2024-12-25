1e3b21f5dcc44f377812f3e7739136c959ba906d
Bruno Chavetta <bruno.chavetta@jala.university>
2024-12-25 02:51:20 -0300

    feat: adding customer data structures

---
+++ src/types/customer.js
@@ -0,0 +1,18 @@
+const Customer = {
+  id: 0,
+  first_name: '',
+  last_name: '',
+  email: '',
+  gender: '',
+  country: '',
+  ip_address: '',
+};
+
+const CustomerInput = {
+  first_name: '',
+  last_name: '',
+  email: '',
+  gender: '',
+  country: '',
+  ip_address: '',
+};
